083b939a944ae3bb7807323ad5390564a1c199f8
adrian <you@example.com>
2022-09-07 23:19:01 -0700

    first committ being made

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int shoeSize; //a person's shoe size
+		shoeSize=11;
+		shoeSize=shoeSize+1; 
+		
+		int temperature = 65;
+		int celcius =(temperature-32)*5/9;
+		System.out.println(temperature);
+	
+		
+		
+		
+		
+	
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+
+
